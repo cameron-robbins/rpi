ce2725976e1bbdb34c0d7f861e0250cddafed521
Cameron Robbins <crobbins799@gmail.com>
2025-06-28 02:15:30 -0400

    Add IP and MAC address display to main function

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ def main():
     print(f"platform: {platform.platform()}")
     print(f"python version: {platform.python_version()}")
     print(f"socket: {socket.gethostname()}")
+    print(f"ip address: {socket.gethostbyname(socket.gethostname())}")
+    print(f"mac address: {':'.join(hex(ord(c))[2:] for c in uuid.getnode().to_bytes(6, 'little'))}")
     
 if __name__ == "__main__":
     main()
